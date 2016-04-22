974104b90b940180871438367b03b127d49fde2c
PatrykM5 <patryk.masiakowski@student.put.poznan.pl>
2016-04-22 23:27:13 +0200

    sensor - not work

--- sensor/main.c
+++ sensor/main.c
@@ -6,20 +6,58 @@
 #include "stm32f4xx_syscfg.h"
 #include "misc.h"
 
-void Timmer_conf(){
+void TRIG_GPIO_conf(){
+		/* GPIOD Periph clock enable */
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+		GPIO_InitTypeDef  GPIO_InitStructure;
+		/* Configure PD1 in output pushpull mode */
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+		GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+		GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+		GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
+		GPIO_Init(GPIOD, &GPIO_InitStructure);
+}
+
+void ECHO_GPIO_conf(){
+		/* GPIOD Periph clock enable */
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+		GPIO_InitTypeDef  GPIO_InitStructure;
+		/* Configure PD1 in output pushpull mode */
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+		GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+		GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+		GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
+		GPIO_Init(GPIOD, &GPIO_InitStructure);
+}
+
+void Timmer2_conf(){
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
 
 	TIM_TimeBaseInitTypeDef Timmer2;
-	Timmer2.TIM_Period = 999; /*1 milisecond*/
+	Timmer2.TIM_Period = 9; /*10 microseconds*/
 	Timmer2.TIM_Prescaler = 83; /*1 microsecond tick*/
 	Timmer2.TIM_ClockDivision = TIM_CKD_DIV1;
 	Timmer2.TIM_CounterMode = TIM_CounterMode_Up;
 	TIM_TimeBaseInit(TIM2, &Timmer2);
 
-	TIM_Cmd(TIM2, ENABLE);
 }
 
-void Przerwania_conf(){
+void Timmer3_conf(){
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+
+		TIM_TimeBaseInitTypeDef Timmer3;
+		Timmer3.TIM_Period = 9; /*999=1 milisecond*/
+		Timmer3.TIM_Prescaler = 83; /*1 microsecond tick*/
+		Timmer3.TIM_ClockDivision = TIM_CKD_DIV1;
+		Timmer3.TIM_CounterMode = TIM_CounterMode_Up;
+		TIM_TimeBaseInit(TIM3, &Timmer3);
+}
+
+void Przerwania2_conf(){
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
 	// ustawienie trybu pracy priorytetow przerwan
 
@@ -34,22 +72,59 @@ void Przerwania_conf(){
 	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
 }
 
-void TIM3_IRQHandler(void){
+void Przerwania3_conf(){
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+	// ustawienie trybu pracy priorytetow przerwan
+
+	NVIC_InitTypeDef przerwania;
+	przerwania.NVIC_IRQChannel = TIM3_IRQn;
+	przerwania.NVIC_IRQChannelPreemptionPriority = 0x00;
+	przerwania.NVIC_IRQChannelSubPriority = 0x00;
+	przerwania.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&przerwania);
+
+	TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
+	TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
+}
+
+int ile=0;
+
+void TIM2_IRQHandler(void){
 	if(TIM_GetITStatus(TIM2,TIM_IT_Update)!=RESET){
-		GPIO_ToggleBits(GPIOD,GPIO_Pin_14);
 		TIM_ClearITPendingBit(TIM2,TIM_IT_Update);
+		GPIO_WriteBit(GPIOD,GPIO_Pin_1,0);
+		TIM_Cmd(TIM2, DISABLE);
 	}
 }
 
-unsigned int counter = TIM2->CNT;
+void TIM3_IRQHandler(void){
+	if(TIM_GetITStatus(TIM3,TIM_IT_Update)!=RESET){
+		TIM_ClearITPendingBit(TIM3,TIM_IT_Update);
+		ile=ile+10;
+	}
+}
+
+unsigned int counter, odleglosc;
 
 int main(void)
 {
-	Timmer_conf();
-	Przerwania_conf();
+	Timmer2_conf();
+	Timmer3_conf();
+	Przerwania2_conf();
+	Przerwania3_conf();
+	TRIG_GPIO_conf();
+	ECHO_GPIO_conf();
 	SystemInit();
+	int i;
     while(1)
     {
-    	counter = TIM2->CNT;
+    	ile=0;
+    	GPIO_WriteBit(GPIOD,GPIO_Pin_1,1);
+    	TIM_Cmd(TIM2,ENABLE);
+    	while(GPIO_ReadInputDataBit(GPIOD,GPIO_Pin_2)==0);
+    	TIM_Cmd(TIM3,ENABLE);
+    	while(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_2)==1);
+    	TIM_Cmd(TIM3,DISABLE);
+    	odleglosc=ile/58;
     }
 }
