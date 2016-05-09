92dc530ddce97ffb882f7ee60632cecb61836239
PatrykM5 <patryk.masiakowski@student.put.poznan.pl>
2016-05-09 16:29:32 +0200

    Revert "sdcard - not working"
    This reverts commit 59c5a6724138325511443ee73e3a0dfdab1dc10a.

--- main.c
+++ main.c
@@ -202,58 +202,6 @@ void display(unsigned int odleglosc,char str[]){
 	    	}
 }
 
-void LED_GPIO_conf(){
-		/* GPIOD Periph clock enable */
-		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-
-		GPIO_InitTypeDef  GPIO_InitStructure;
-		/* Configure PD12, PD13, PD14 and PD15 in output pushpull mode */
-		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
-		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-		GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-		GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-		GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-		GPIO_Init(GPIOD, &GPIO_InitStructure);
-}
-
-void sdcard(){
-	//Fatfs object
-	FATFS FatFs;
-	//File object
-	FIL fil;
-	//Free and total space
-	uint32_t total, free;
-	TM_DELAY_Init();
-	LED_GPIO_conf();
-	if (f_mount(&FatFs, "", 1) == FR_OK) {
-	        //Mounted OK, turn on RED LED
-	        GPIO_SetBits(GPIOD,GPIO_Pin_14);
-
-	        //Try to open file
-	        if (f_open(&fil, "textfile.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE) == FR_OK) {
-	            //File opened, turn off RED and turn on GREEN led
-	        	GPIO_SetBits(GPIOD,GPIO_Pin_12);
-	            GPIO_ResetBits(GPIOD,GPIO_Pin_14);
-
-	            //If we put more than 0 characters (everything OK)
-	            if (f_puts("First string in my file\n", &fil) > 0) {
-	                if (TM_FATFS_DriveSize(&total, &free) == FR_OK) {
-	                    //Data for drive size are valid
-	                }
-
-	                //Turn on both leds
-	                GPIO_SetBits(GPIOD,GPIO_Pin_12|GPIO_Pin_14);
-	            }
-
-	            //Close file
-	            f_close(&fil);
-	        }
-
-	        //Unmount drive
-	        f_mount(0, "", 1);
-	    }
-}
-
 unsigned int odleglosc;
 
 int main(void) {
@@ -277,7 +225,6 @@ int main(void) {
     TRIG_GPIO_conf();
     ECHO_GPIO_conf();
     SystemInit();
-    sdcard();
     GPIOD->BSRRH = GPIO_Pin_1;
 
     char str[100];
